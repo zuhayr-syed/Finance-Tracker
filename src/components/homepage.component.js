59206004b481c26b100fc515ba4528f24559ad48
Zuhayr Syed <zuhayr.syed@Zuhayrs-MacBook-Pro.local>
2021-09-11 13:38:10 -0400

    page background colours

--- src/components/homepage.component.js
+++ src/components/homepage.component.js
@@ -4,14 +4,14 @@ import React, { Component } from 'react';
 export default class HomePage extends Component {
     render () {
         return (
-            <div>
+            <div className="mt-3 bg-secondary text-white p-5">
                 <head>
                     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link>
                 </head>
                 <h1 className="text-center">
                 Welcome to the Finance Tracker! 
                 </h1>
-                <h5>Use this application to keep track of your: </h5>
+                <h5 className="mt-5">Use this application to keep track of your: </h5>
                 <ul>
                     <li>Tuition</li>
                     <li>Expenses</li>
